b6e25e3d674f9d3be18c1628b95a75fcf3017031
BoraAbramov <borya812@gmail.com>
2026-02-14 19:06:26 +0200

    task 2 complete

--- task 2 - list backup and clearing.py
+++ task 2 - list backup and clearing.py
@@ -1,2 +1,21 @@
 
 grades = [85, 90, 78, 92, 88]
+
+# section 1 + 2
+
+grades2 = grades.copy()
+grades.clear()
+
+#section 3
+
+print(f"grades list {grades}")
+print(f"grades2 list {grades2}")
+
+#section 4
+
+numbers = [95, 100]
+
+print(f"grades2 + jbl {grades2 + numbers}")
+
+grades2.extend(numbers)
+print(f"grades2 extend {grades2}")
