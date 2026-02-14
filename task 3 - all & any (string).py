dc774a4448c75308a59c5cd9cccead3fd421ab17
BoraAbramov <borya812@gmail.com>
2026-02-14 19:43:21 +0200

    task 3 complete

--- task 3 - all & any (string).py
+++ task 3 - all & any (string).py
@@ -1,2 +1,11 @@
+from asyncio.unix_events import can_use_pidfd
 
 words = ["HELLO", "WORLD", "PYTHON", "CODE", "DEVELOPER", "AI"]
+
+#section 1
+
+print(f"all words in the string are use upper letters? {all(bigword.isupper for bigword in words)}")
+
+#section 2
+
+print(f"is there any word with length 5 or more characters? {any(len(oreh) > 5 for oreh in words)}")
